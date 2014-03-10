680844ffe547d6f42f4d724045ea8246be947e5a
jmslbam <jmslbam@gmail.com>
2014-03-10 19:25:04 +0100

    Added wp-config-env.php

---
+++ www/wp-config-env-sample.php
@@ -0,0 +1,14 @@
+<?php
+
+define( 'LOCAL_DEV', true );
+define( 'WP_DEBUG', true );
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'xyz');
+
+/** MySQL database username */
+define('DB_USER', 'xyz');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'xzy');
